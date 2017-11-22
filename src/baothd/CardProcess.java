36e1c7bd127e6f90d5f673e3718798da9ba2d4cd
baothdse <baothdse@gmail.com>
2017-11-22 15:50:24 +0700

    Fix main class

--- src/baothd/CardProcess.java
+++ src/baothd/CardProcess.java
@@ -1,10 +1,45 @@
 package baothd;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * @author USER
+ *
+ */
+/**
+ * @author USER
+ *
+ */
 public class CardProcess {
+	
+	/**
+	 * @param input
+	 * @return List<Card> or Null if input is invalid
+	 */
+	public List<Card> changeInputToCards(String input) {
+		String regex = "(S|H|D|C)([2-9]|10|J|Q|K|A)";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(input);
+		List<Card> cards = new ArrayList<Card>();
+		while (matcher.find()) {
+			String str = matcher.group();
+			Card card = new Card(str.substring(0, 1), str.split(str.substring(0, 1))[1]);
+			// System.out.println(card);
+			cards.add(card);
+		}
+		cards = (cards.size() != 5) ? null : cards;
+		return cards;
+	}
+	
+	/**
+	 * Use to determine how many pair, three same cards and four same cards the input have.
+	 * @param cards
+	 * @param input
+	 * @return Result
+	 */
 	public Result cardProcess(List<Card> cards, String input) {
 		String regex = "";
 		int pair = 0, threeCards = 0, fourCards = 0;
@@ -27,10 +62,17 @@ public class CardProcess {
 		return result;
 	}
 	
+	
+	/**
+	 * Determine the string after get result.
+	 * @param cards
+	 * @param input
+	 * @return String
+	 */
 	public String showResult(List<Card> cards, String input) {
 		Result result = cardProcess(cards, input);
 		//System.out.println(result);
-		String resultString = "";
+		String resultString = "--";
 		if(result.getFourCards() == 0) {
 			if(result.getThreeCards() == 1){
 				resultString = (result.getPair() == 1) ? "FH" : "3C";
@@ -41,7 +83,7 @@ public class CardProcess {
 					resultString = "1P";
 				}
 			}
-		} else {
+		} else if (result.getFourCards() == 1){
 			resultString = "4C";
 		}
 		return resultString;
--- src/baothd/Hand.java
+++ src/baothd/Hand.java
@@ -1,10 +1,7 @@
 package baothd;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Hand {
 	public static void main(String[] args) {
@@ -13,19 +10,9 @@ public class Hand {
 		while (!cont.equals("N")) {
 			System.out.println("Please input your card!");
 			String input = scanner.nextLine();
-			String regex = "(S|H|D|C)([2-9]|10|J|Q|K|A)";
-			Pattern pattern = Pattern.compile(regex);
-			Matcher matcher = pattern.matcher(input);
-			List<Card> cards = new ArrayList<Card>();
 			CardProcess cardProcess = new CardProcess();
-			while (matcher.find()) {
-				String str = matcher.group();
-				Card card = new Card(str.substring(0, 1), str.split(str.substring(0, 1))[1]);
-				// System.out.println(card);
-				cards.add(card);
-			}
-			boolean isValid = (cards.size() != 5) ? false : true;
-			if (isValid == true) {
+			List<Card> cards = cardProcess.changeInputToCards(input);
+			if (cards != null) {
 				String result = cardProcess.showResult(cards, input);
 				System.out.println("Result: " + result);
 			} else {
